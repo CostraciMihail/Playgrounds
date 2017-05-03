4193072e850489a11f7d3c5c823af7ef68c7e5ca
Costraci Mihail <mihaicostraci3@gmail.com>
2017-05-03 18:41:23 +0300

    Add space animation.

--- LineJoinStyle.playground/Contents.swift
+++ LineJoinStyle.playground/Contents.swift
@@ -17,5 +17,52 @@ PlaygroundPage.current.liveView = containerView
 
 
 //POINTS
-let pointsAnimation = Points()
-pointsAnimation.animate(inView: containerView)
+//let pointsAnimation = Points()
+//pointsAnimation.animate(inView: containerView)
+
+
+//space
+containerView.backgroundColor = UIColor.black
+
+var circlePath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667),
+                                             y: Int.random(lower: 0, upper: 667),
+                                             width: 5,
+                                             height: 5))
+
+
+for _ in 0..<100 {
+    
+    let circleSize = Int.random(lower: 0, upper: 50)
+    let tmpPath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667),
+                                              y: Int.random(lower: 0, upper: 667),
+                                              width: circleSize,
+                                              height: circleSize))
+    circlePath.append(tmpPath)
+}
+
+
+let circleLayer = CAShapeLayer(layer: containerView.layer)
+circleLayer.path = circlePath.cgPath
+circleLayer.fillColor = UIColor.red.cgColor
+containerView.layer.addSublayer(circleLayer)
+
+
+var emptyPath = UIBezierPath(ovalIn: CGRect(x: 375/2, y: 667/2, width: 0, height: 0))
+
+
+let emptyLayer = CAShapeLayer(layer: containerView.layer)
+emptyLayer.path = emptyPath.cgPath
+emptyLayer.fillColor = UIColor.red.cgColor
+containerView.layer.addSublayer(circleLayer)
+
+
+let spaceAnim = CABasicAnimation(keyPath: "path")
+spaceAnim.duration = 10.0
+spaceAnim.fromValue = emptyLayer.path
+spaceAnim.toValue = circleLayer.path
+spaceAnim.isRemovedOnCompletion = false
+spaceAnim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+
+circleLayer.add(spaceAnim, forKey: "spaceAnimation")
+
+
