a943ba76b132eb0564681229f6bd738180e01937
Costraci Mihail <mihaicostraci3@gmail.com>
2017-05-04 12:06:18 +0300

    Finish spcace animation.

--- LineJoinStyle.playground/Contents.swift
+++ LineJoinStyle.playground/Contents.swift
@@ -10,18 +10,18 @@ let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 6
 containerView.backgroundColor = UIColor.white
 PlaygroundPage.current.liveView = containerView
 
-//LINE
+// 1. LINE
 //let lineAnimation = Line(inContainerView: containerView)
 //lineAnimation.widthAnimation()
 //lineAnimation.springWidthAnimation()
 
 
-//POINTS
+//2. POINTS
 //let pointsAnimation = Points()
 //pointsAnimation.animate(inView: containerView)
 
 
-//space
+//3. SPACE
 containerView.backgroundColor = UIColor.black
 
 var circlePath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667),
@@ -30,9 +30,9 @@ var circlePath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667)
                                              height: 5))
 
 
-for _ in 0..<100 {
+for _ in 0..<600 {
     
-    let circleSize = Int.random(lower: 0, upper: 50)
+    let circleSize = Int.random(lower: 0, upper: 15)
     let tmpPath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667),
                                               y: Int.random(lower: 0, upper: 667),
                                               width: circleSize,
@@ -49,6 +49,16 @@ containerView.layer.addSublayer(circleLayer)
 
 var emptyPath = UIBezierPath(ovalIn: CGRect(x: 375/2, y: 667/2, width: 0, height: 0))
 
+for _ in 0..<600 {
+    
+//    let circleSize = Int.random(lower: 0, upper: 50)
+    let tmpPath = UIBezierPath(ovalIn: CGRect(x: Int.random(lower: 0, upper: 667),
+                                              y: Int.random(lower: 0, upper: 667),
+                                              width: 0,
+                                              height: 0))
+    emptyPath.append(tmpPath)
+}
+
 
 let emptyLayer = CAShapeLayer(layer: containerView.layer)
 emptyLayer.path = emptyPath.cgPath
@@ -57,11 +67,29 @@ containerView.layer.addSublayer(circleLayer)
 
 
 let spaceAnim = CABasicAnimation(keyPath: "path")
-spaceAnim.duration = 10.0
+spaceAnim.duration = 5.0
 spaceAnim.fromValue = emptyLayer.path
 spaceAnim.toValue = circleLayer.path
-spaceAnim.isRemovedOnCompletion = false
-spaceAnim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+spaceAnim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+spaceAnim.repeatCount = 10
+spaceAnim.autoreverses = true
+
+
+// UNCOMMNET TO ADD FILL COLOR AND GROUP ANIMATION .
+//let colorAnim = CABasicAnimation(keyPath: "fillColor")
+//colorAnim.duration = 5.0
+//colorAnim.fromValue = UIColor.red.cgColor as Any
+//colorAnim.toValue = UIColor.white.cgColor as Any
+//colorAnim.repeatCount = 10
+//colorAnim.autoreverses = true
+//colorAnim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+
+
+//let groupAnim = CAAnimationGroup()
+//groupAnim.duration = 10.0
+//groupAnim.repeatCount = 10
+//groupAnim.autoreverses = true
+//groupAnim.animations = [spaceAnim, colorAnim]
 
 circleLayer.add(spaceAnim, forKey: "spaceAnimation")
 
